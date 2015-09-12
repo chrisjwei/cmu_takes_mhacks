5342af1afe024856eb4562e58f9f687ce8fb8990
raoariel <arielrao@andrew.cmu.edu>
2015-09-12 06:22:08 -0400

    ar/consolidate modules to single call

---
+++ app/nlp/FeedContent.py
@@ -0,0 +1,29 @@
+from parse import *
+from summarize import *
+from categorize.get_keywords import *
+
+class FeedContent:
+  def __init__(self,sum_len=2):
+    self.summary_len = sum_len
+    self.summarizer = FrequencySummarizer("SmartStoplist.txt")
+    self.categorizer = CategorizeNewsArticle()
+    # search terms
+    #self.getSearchResults()
+    self.getContent()
+    pass
+
+  # return list of urls to parse
+  def getSearchResults(self): pass
+
+  def getContent(self):
+    # loop through articles to parse
+    articleContent = {}
+    url=u'http://www.nydailynews.com/news/politics/rudy-giuliani-people-living-shelters-aren-home-article-1.2357767/'
+    parser = ParseArticle(url)
+    articleContent["article"] = parser.getArticleData()
+    articleText = articleContent["article"]["text"]
+
+    articleContent["summary"] = self.summarizer.summarize(articleText,self.summary_len)
+
+    articleContent["keywords"] = self.categorizer.run(articleText)
+    return articleContent
