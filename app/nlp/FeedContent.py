f3084bb3751b6dd40eff8715133a60b2e327ed4e
raoariel <arielrao@andrew.cmu.edu>
2015-09-12 08:14:02 -0400

    ar/minor changes for making call to google api

--- app/nlp/FeedContent.py
+++ app/nlp/FeedContent.py
@@ -1,29 +1,55 @@
+import json, requests
 from parse import *
 from summarize import *
 from categorize.get_keywords import *
 
 class FeedContent:
-  def __init__(self,sum_len=2):
+  def __init__(self,keywords,sum_len=2):
+    self.articles = []
+    self.rawData = []
+    self.url = "https://ajax.googleapis.com/ajax/services/search/news?v=1.0&rsz=8"
     self.summary_len = sum_len
     self.summarizer = FrequencySummarizer()
     self.categorizer = CategorizeNewsArticle()
+    ## loop through params,  and make separate calls, update same obj.
+    self.encodeParams() 
     # search terms
-    #self.getSearchResults()
+    self.getSearchResults()
     self.getContent()
     pass
 
+  def encodeParams(self): 
+    # pass look keywords
+    self.encodedParams = "homeless"
+    return 
+
   # return list of urls to parse
-  def getSearchResults(self): pass
+  def getSearchResults(self): 
+    pass
+    full_url = self.url + "&q=%s" % self.encodedParams
+    response = requests.get(full_url)
+    data = json.loads(response.text)
+    self.rawData = data["responseData"]["results"]
+    
+  def processContent(self):
+    for article in self.rawData:
+      articleContent = {}
+      url = article[u'unescapedUrl']
+      parser = ParseArticle(url)
+      articleContent["article"] = parser.getArticleData()
+      try: articleContent["article"]["language"] = article["language"]
+      except: articleContent["article"]["language"] = None
+      try: articleContent["article"]["location"] = article["location"]
+      except: articleContent["article"]["location"] = None
+      try: articleContent["article"]["image"] = article["image"]
+      except: articleContent["article"]["image"] = None
+      articleText = articleContent["article"]["text"]
+
+      articleContent["summary"] = self.summarizer.summarize(articleText,self.summary_len)
+
+      articleContent["keywords"] = self.categorizer.run(articleText)
+      self.articles.append(articleContent)
+    return
 
   def getContent(self):
-    # loop through articles to parse
-    articleContent = {}
-    url=u'http://www.nydailynews.com/news/politics/rudy-giuliani-people-living-shelters-aren-home-article-1.2357767/'
-    parser = ParseArticle(url)
-    articleContent["article"] = parser.getArticleData()
-    articleText = articleContent["article"]["text"]
-
-    articleContent["summary"] = self.summarizer.summarize(articleText,self.summary_len)
-
-    articleContent["keywords"] = self.categorizer.run(articleText)
-    return articleContent
+    return {"articles": self.articles}
--- app/nlp/categorize/get_keywords.py
+++ app/nlp/categorize/get_keywords.py
@@ -139,7 +139,7 @@ class CategorizeNewsArticle(object):
         self.stop_words_pattern = build_stop_word_regex(stop_words_path)
         pass
 
-    def run(self, text="", max_keywords=6):
+    def run(self, text="", max_keywords=3):
         if text == "":
             text = u"Bryan R. Smith/AP The former mayor took issue with de Blasio\u2019s comment that homelessness rose 40% during the Giuliani administration \u2014 saying that actually the shelter population rose only 32%.\n\nIt all depends on what you mean by \u201chomeless.\u201d\n\nRudy Giuliani, defending his City Hall record in an ongoing spat with Mayor de Blasio, said Friday shelter residents don\u2019t count as homeless \u2014 a definition that would surprise the Department of Homeless Services.\n\nThe former mayor took issue with de Blasio\u2019s comment that homelessness rose 40% during the Giuliani administration \u2014 saying that actually the shelter population rose only 32%.\n\n\u201cHere's how ignorant Mayor de Blasio is ... people who are in shelters, I would like to inform the mayor, are not homeless,\u201d said Giuliani in an interview on \u201cGood Day New York\u201d before going to the 9/11 Memorial.\n\nSpencer Platt/Getty Images 'Here's how ignorant Mayor de Blasio is ... people who are in shelters, I would like to inform the mayor, are not homeless,' said Rudy Giuliani in an interview.\n\nA spokeswoman for the mayor said name-calling on the 9/11 anniversaries was \u201cinappropriate\u201d and declined further comment.\n\nHomeless advocates were incensed. \u201cOf course people in homeless shelters are homeless,\u201d said Jennifer Flynn of Vocal-NY."
         try: 
