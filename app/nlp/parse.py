9aa95539d36c3fda1f293f70460ebf82bdb206ff
raoariel <arielrao@andrew.cmu.edu>
2015-09-12 04:13:25 -0400

    ar/add module for parsing url to object with text and article information

---
+++ app/nlp/parse.py
@@ -0,0 +1,23 @@
+from newspaper import Article
+
+class ParseArticle:
+  def __init__(self, url=u'http://www.nydailynews.com/news/politics/rudy-giuliani-people-living-shelters-aren-home-article-1.2357767/'):
+    self.url = url
+
+  def getArticleData(self):
+    article = Article(self.url)
+    article.download()
+    article.html
+    article.parse()
+
+    articleData = {}
+    articleData["title"] = article.title
+    articleData["authors"] = article.authors  # list
+    articleData["publish_data"] = article.publish_date  # date obj?
+    articleData["text"] = article.text  # unicode
+
+    # article.nlp()
+    # article.summary
+    # article.keywords
+    return articleData
+
